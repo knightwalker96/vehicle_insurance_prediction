f276a3529bdb735d4e384d0b445ef7c6000bbc76
knightwalker96 <soumyodeepdey96@gmail.com>
2025-08-31 23:25:05 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anamitra Singha",
+    author_email="saheb123.singha0@gmail.com",
+    packages=find_packages()
+)
