9d8ae4ab204a7bfa4f47476122ef2cca1228cd3c
Soumyodeep Dey <50855949+knightwalker96@users.noreply.github.com>
2025-10-26 19:40:10 +0530

    Update author information in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Anamitra Singha",
-    author_email="saheb123.singha0@gmail.com",
+    author="Soumyodeep Dey",
+    author_email="soumyodeepdey960@gmail.com",
     packages=find_packages()
 )
